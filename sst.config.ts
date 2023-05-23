46ced29bc3fe1ea434c33eba36133b6ba1122499
vermontGuy <sean.neuffer@gmail.com>
2023-05-22 20:21:21 -0400

    Adding a storage stack

--- sst.config.ts
+++ sst.config.ts
@@ -1,14 +1,13 @@
 import { SSTConfig } from "sst";
-import { API } from "./stacks/MyStack";
-
+import { StorageStack } from "./stacks/StorageStack";
 export default {
-  config(_input) {
-    return {
-      name: "notes",
-      region: "us-east-1",
-    };
-  },
-  stacks(app) {
-    app.stack(API);
-  }
+config(_input) {
+return {
+name: "notes",
+region: "us-east-1",
+};
+},
+stacks(app) {
+app.stack(StorageStack);
+},
 } satisfies SSTConfig;
